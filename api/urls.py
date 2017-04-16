6059f8b468a602b9a1010c9b24e8bef4b052184b
YogiAi <ayb854@163.com>
2017-04-16 09:23:04 +0800

    add graph

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^point_detail', views.point_detail, name='point_detail'),
+    url(r'^points', views.points, name='points'),
+    url(r'^boundaries', views.boundaries, name='boundaries'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,43 @@
 # Create your views here.
+from django.db.models import Avg, Max
 from django.http import JsonResponse
 from map.models import *
+import ast
+
+def boundaries(request):
+    geojson = {'type': "FeatureCollection"}
+
+    geo_list = []
+    avg_list = []
+    for geo_info in Geoinfo.objects.all():
+        properties = ast.literal_eval(geo_info.properties)
+        geometry = ast.literal_eval(geo_info.geometry)
+
+        # query from database
+        properties['avg_roi'] = RoiResult.objects.filter(district=geo_info.district).aggregate(Avg('roi')).get(
+            'roi__avg')
+        properties['max_roi'] = RoiResult.objects.filter(district=geo_info.district).aggregate(Max('roi')).get(
+            'roi__max')
+
+        if properties['avg_roi'] is not None:
+            avg_list.append(properties['avg_roi'])
+
+        feature = {'type': "Feature", 'properties': properties, 'geometry': geometry}
+        geo_list.append(feature)
+
+    geojson['features'] = geo_list
+    return JsonResponse(geojson)
+
+def points(request):
+    roi_results = RoiResult.objects.all()
+
+    points = list()
+    for roi_result in roi_results:
+        if roi_result.latitude == 0 or roi_result.longitude == 0:
+            continue
+        points.append([roi_result.latitude, roi_result.longitude, str(roi_result.id)])
+
+    return JsonResponse(points, safe=False)
 
 def point_detail(request):
     roi_id = request.GET.get('roi_id', '')
